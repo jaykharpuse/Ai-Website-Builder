febc0b37d8d91390c042170dab4d20dfecf5df09
jaykharpuse <jaykharpuse51@gmail.com>
2025-12-26 13:00:04 +0530

    first commit

---
+++ convex/schema.js
@@ -0,0 +1,18 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    name: v.string(),
+    email: v.string(),
+
+    // Clerk fields
+    imageUrl: v.optional(v.string()),
+    tokenIdentifier: v.optional(v.string()),
+  }),
+
+  workspace: defineTable({
+    messages: v.any(),
+    fileData: v.optional(v.any()),
+  }),
+});
